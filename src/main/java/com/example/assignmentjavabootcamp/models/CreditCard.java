5980f5e3dbae3944ba2e02a9428dcd40a6383bcc
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2022-02-19 13:40:36 +0700

    [card-77941916] improve import pacakge

--- src/main/java/com/example/assignmentjavabootcamp/models/CreditCard.java
+++ src/main/java/com/example/assignmentjavabootcamp/models/CreditCard.java
@@ -8,11 +8,9 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Getter
