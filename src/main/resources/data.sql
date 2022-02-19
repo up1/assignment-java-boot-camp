6da567b262ece1aa16f7e75a58641918f70633df
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2022-02-19 14:28:26 +0700

    [card-77939895] rename data.sql table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into Customer (customer_id, firstname, lastname, address, zip_code, province, phone_number)
+insert into Customers (customer_id, firstname, lastname, address, zip_code, province, phone_number)
 Values (1, 'Tom', 'Hank', '38 Moo 14', '10140', 'Bangkok', '09111111');
 
-insert into Product(product_id, name,type_of_size, size,color ,price, amount)
+insert into Products(product_id, name,type_of_size, size,color ,price, amount)
 values (1,'Adidas RUNNING Roamer FY6698','UK','9','Black',1350,15),
        (2,'Adidas Yeezy 700 V2 Runner Boost Unisex B75571','EU','8','Red',2800,14),
        (3,'Pharrell x NMD Human Race','EU','36','Yellow',1190,20),
