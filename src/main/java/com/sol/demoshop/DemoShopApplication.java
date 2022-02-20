79364dade266e21790a2fd5e23a89ddf79937159
faisolchehumar <faisol.che@knstrans.com>
2022-02-20 16:01:41 +0700

    Setup Springboot and dependencies #3

---
+++ src/main/java/com/sol/demoshop/DemoShopApplication.java
@@ -0,0 +1,13 @@
+package com.sol.demoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sol/demoshop/DemoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sol.demoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
