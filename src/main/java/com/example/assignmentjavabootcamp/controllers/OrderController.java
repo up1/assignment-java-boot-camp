258c97e0c3f521498d11b7d4f8f9d98f9c6c3969
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2022-02-23 00:58:58 +0700

    add endpoint get order by orderId

--- src/main/java/com/example/assignmentjavabootcamp/controllers/OrderController.java
+++ src/main/java/com/example/assignmentjavabootcamp/controllers/OrderController.java
@@ -14,10 +14,17 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @GetMapping
+    @GetMapping("/lastest")
     public ResponseEntity<?> getLatestOrder(@RequestParam Long customerId) {
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(orderService.getLatestOrder(customerId).toString());
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<?> getOrderById(@PathVariable Long orderId) {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(orderService.getOrderById(orderId).toString());
     }
 }
