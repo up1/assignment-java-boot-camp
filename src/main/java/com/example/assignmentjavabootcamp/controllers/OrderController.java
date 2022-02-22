bda37da442dcc1fab68855bc0f328c7c56732ba8
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2022-02-22 15:42:08 +0700

    add OrderController

---
+++ src/main/java/com/example/assignmentjavabootcamp/controllers/OrderController.java
@@ -0,0 +1,23 @@
+package com.example.assignmentjavabootcamp.controllers;
+
+import com.example.assignmentjavabootcamp.services.OrderService;
+import lombok.AllArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<?> getLatestOrder(@RequestParam Long customerId) {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(orderService.getLatestOrder(customerId).toString());
+    }
+}
