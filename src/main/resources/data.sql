fcdfd159a2d771b3ede81a716832efb283061666
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2022-02-23 11:26:14 +0700

    increase item amount

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,11 +2,11 @@ insert into Customers (customer_id, firstname, lastname, address, zip_code, prov
 Values (1, 'Tom', 'Hank', '38 Moo 14', '10140', 'Bangkok', '0911111111');
 
 insert into Products(product_id, name,type_of_size, size,color ,price, amount)
-values (1,'Adidas RUNNING Roamer FY6698','UK','9','Black',1350,15),
-       (2,'Adidas Yeezy 700 V2 Runner Boost Unisex B75571','EU','8','Red',2800,14),
-       (3,'Pharrell x NMD Human Race','EU','36','Yellow',1190,20),
-       (4,'Adidas ORIGINALS ZX 700 HD FX5812','UK','11.5','Black',3400,42),
-       (5,'Adidas ORIGINALS NMD R1 B42200','UK','3.5','Black',3588,21);
+values (1,'Adidas RUNNING Roamer FY6698','UK','9','Black',1350,100),
+       (2,'Adidas Yeezy 700 V2 Runner Boost Unisex B75571','EU','8','Red',2800,100),
+       (3,'Pharrell x NMD Human Race','EU','36','Yellow',1190,100),
+       (4,'Adidas ORIGINALS ZX 700 HD FX5812','UK','11.5','Black',3400,100),
+       (5,'Adidas ORIGINALS NMD R1 B42200','UK','3.5','Black',3588,100);
 
 select 1
 from dual;
